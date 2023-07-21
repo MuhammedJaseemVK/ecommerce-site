3fae649956c4e29354d2223bc89b7496865f8e76
Jaseem <muhammedjaseemvk96@gmail.com>
2023-07-21 11:00:48 +0530

    simple card created

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Home.css'
+import { getAllProducts } from '../../ApiService/api'
 
 function Home() {
+
+    const [products, setproducts] = useState([])
+    
+    useEffect(() => {
+
+        const fetchProducts = async () =>{
+            const data = await getAllProducts();
+            console.log(data,'data')
+        }
+        fetchProducts();
+    }, [])
+    
+
   return (
-    <div>
-        Home
+    <div className='product-grid'>
+        <div className='product'>
+            <img src='' alt='alternative'/>
+            <h2>Sample</h2>
+            <p>
+
+                <span className='price'>10</span>
+            </p>
+            <button className='product-btn'>Product details</button>
+        </div>
     </div>
   )
 }
